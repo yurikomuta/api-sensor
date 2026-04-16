4ba886b3bf3ea869245f3c2a700b554c8ee5973c
Yuri Komuta <33011150+yurikomuta@users.noreply.github.com>
2026-04-16 12:40:17 +0000

    API sensor

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+app.get("/sensor", (req, res) => {
+  const { temp, hum } = req.query;
+
+  if (!temp || !hum) {
+    return res.status(400).send("Dados inválidos");
+  }
+
+  console.log(`Temp: ${temp} | Umidade: ${hum}`);
+
+  res.send("OK");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {scrollX
+  console.log("Servidor rodando");
+});
